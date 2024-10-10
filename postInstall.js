f707c6561138200cf40a500fcaae9c04545d63bd
dariodds <dario.dasilva.private@gmail.com>
2024-10-10 22:02:29 +0200

    post install adding entries into gitignore

--- postInstall.js
+++ postInstall.js
@@ -1,9 +1,38 @@
-const fs = require('fs-extra')
-const path = require('path')
+const fsExtra = require('fs-extra');
+const fs = require('fs');
+const path = require('path');
 
-const source = path.join(process.cwd(),"src/Core")
-const destination = '../../src';
-console.log(`copying NSEMEA core library from ${source} to ${destination}`)
-fs.copy(source,destination,{ preserveTimestamps:true}, err => {
+const source = path.join(process.cwd(),"src/Core");
+const destination = '../../src/Core';
+console.log(`copying NSEMEA core library from ${source} to ${destination}`);
+fsExtra.copy(source,destination,{ preserveTimestamps:true}, err => {
   console.error(err);
+});
+
+const gitignorePath = path.resolve(__dirname, '../.gitignore');
+const entriesToAdd = ['./src/Core'];
+
+console.log(`gitignorePath ${gitignorePath}`);
+
+fs.readFile(gitignorePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error(`Error reading .gitignore file: ${err}`);
+    return;
+  }
+
+  const existingEntries = data.split('\n');
+  const newEntries = entriesToAdd.filter(entry => !existingEntries.includes(entry));
+
+  if (newEntries.length > 0) {
+    const updatedContent = `${data.trim()}\n${newEntries.join('\n')}\n`;
+    fs.writeFile(gitignorePath, updatedContent, 'utf8', err => {
+      if (err) {
+        console.error(`Error writing to .gitignore file: ${err}`);
+      } else {
+        console.log('Successfully updated .gitignore file.');
+      }
+    });
+  } else {
+    console.log('.gitignore file already contains the specified entries.');
+  }
 });
